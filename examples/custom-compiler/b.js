6236ad4df28ace95e8b74d5e3a23edb146df662b
Osama Abbas <pw.osama@gmail.com>
2019-01-18 02:11:50 +0900

    change examples/custom-compiler to the modified interface

--- examples/custom-compiler/b.js
+++ examples/custom-compiler/b.js
@@ -2,6 +2,8 @@
 
 console.log('__filename: ' + __filename);
 
+var data = require('./data');
+
 module.exports = {
   file: __filename
 };
--- examples/custom-compiler/compiler.js
+++ examples/custom-compiler/compiler.js
@@ -2,13 +2,12 @@
 
 const fs = require('fs');
 
-const compiler = function(filename) {
-  
-  let src = fs.readFileSync(filename, 'utf-8');
-
-  return '/* custom-compiler */\n\n' + src;
+module.exports = {
+  getSupportedExtensions() {
+    return ['.json'];
+  },
+  compile(filename) {
+    let src = fs.readFileSync(filename, 'utf-8');
+    return '/* custom compiler */\nmodule.exports = ' + src + ';';   
+  }
 };
-
-compiler.extname = '.js';
-
-module.exports = compiler;
